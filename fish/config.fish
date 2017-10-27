176d05c11993dbf3ac14242bb6d24105234c02df
Haojun Lu <haojunlu@foxmail.com>
2017-10-27 17:13:56 -0400

    an alias file

--- fish/config.fish
+++ fish/config.fish
@@ -1,31 +1,9 @@
 set fish_greeting ""
 bash ~/workspace/ballista/helper/bash/greet.sh
-
-alias cff="clang-format-3.6  --style=file"
-alias cffja="clang-format-3.6  --style=file -i *.java"
-alias cffc=" clang-format-3.6  --style=file -i *.c " 
-alias cffpp="clang-format-3.6  --style=file -i *.cpp " 
-alias cffh="clang-format-3.6  --style=file -i *.h " 
-alias a='fc -s'
-alias e='exit'
-alias c='javac *.java'
-alias gp='git pull'
-alias heads='head -10'
-alias tails='tail -10'
-alias ww='cd ~/workspace'
-alias cdd='cd ~'
-alias qq='cd ../'
-alias qqq='cd ../../'
-alias qqqq='cd ../../../'
-alias sd='shutdown'
-alias LS='ls'
-alias wd='cd /media/lst2016/Windows/'
-alias cl='xclip'
-alias cll='xclip -o'
-alias clc='xclip -sel clip'
-
+.  ~/workspace/ballista/helper/bash/alias.sh
 eval (thefuck --alias | tr '
-		' ';')
+' ';')
+
 
 set num_faces 6
 set face "☀ "
---
+++ helper/bash/alias.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#####  ALIAS #### 
+
+#clang format check
+alias cff="clang-format-3.6  --style=file"
+alias cffja="clang-format-3.6  --style=file -i *.java"
+alias cffc=" clang-format-3.6  --style=file -i *.c " 
+alias cffpp="clang-format-3.6  --style=file -i *.cpp " 
+alias cffh="clang-format-3.6  --style=file -i *.h " 
+
+
+
+#previous command
+
+alias a='fc -s'
+
+#just because i am lasy
+alias e='exit'
+alias c='javac *.java'
+alias gp='git pull'
+alias heads='head -10'
+alias tails='tail -10'
+
+# walk quickly
+alias ww='cd ~/workspace'
+alias cdd='cd ~'
+alias qq='cd ../'
+alias qqq='cd ../../'
+alias qqqq='cd ../../../'
+
+
+alias sd='shutdown'
+alias LS='ls'
+alias wd='cd /media/lst2016/Windows/'
+# control c 
+alias cl='xclip'
+alias cll='xclip -o'
+alias clc='xclip -sel clip'
+
+# eval $(thefuck --alias) 
