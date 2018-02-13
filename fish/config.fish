310d983184aae25957904b46ef92697863bfbc50
Haojun Lu <haojunlu@foxmail.com>
2018-02-13 00:35:54 -0500

    change rainbow to bg

--- fish/config.fish
+++ fish/config.fish
@@ -19,26 +19,27 @@ set myname (whoami)
 function fish_prompt
 
 	# Name
-	set_color 9400d3
-	echo  -e "$myname \c"
-	set_color 4B0082
-	echo  -e "@ \c"
+	set_color 000000
+	set_color -b 9400d3
+	echo  -e "$myname\c"
+	set_color -b 4B0082
+	echo  -e "@\c"
 	
 	# Time
-	set_color blue
-	echo -e  (date "+%m/%d") "\c"
+	set_color  -b blue
+	echo -e  (date "+%m/%d")"\c"
 	
-	set_color green
-	echo -e  (date "+%a") "\c"
+	set_color -b green
+	echo -e  (date "+%a")"\c"
 	
-	set_color yellow
+	set_color -b yellow
 	echo -e  (date "+%H:%M:%S")"\c"
 	
-	set_color FF8C00
+	set_color -b EE8C00
 	echo -e "+1s⌚⌚ \c"
 	
 	# PWD
-	set_color red
+	set_color -b red
 	echo -n  (prompt_pwd)
 	set_color normal
 	
