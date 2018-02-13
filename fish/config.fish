25cde87acb2816296312d20dfc92ed86efad1ec6
Haojun Lu <haojunlu@foxmail.com>
2018-02-12 23:49:50 -0500

    add tab to fish_prompt

--- fish/config.fish
+++ fish/config.fish
@@ -18,37 +18,38 @@ set myname (whoami)
 
 function fish_prompt
 
-# Name
-set_color 9400d3
-echo  -e "$myname \c"
-set_color 4B0082
-echo  -e "@ \c"
+	# Name
+	set_color 9400d3
+	echo  -e "$myname \c"
+	set_color 4B0082
+	echo  -e "@ \c"
+	
+	# Time
+	set_color blue
+	echo -e  (date "+%m/%d") "\c"
+	
+	set_color green
+	echo -e  (date "+%a") "\c"
+	
+	set_color yellow
+	echo -e  (date "+%H:%M:%S")"\c"
+	
+	set_color FF8C00
+	echo -e "+1s⌚⌚ \c"
+	
+	# PWD
+	set_color red
+	echo -n  (prompt_pwd)
+	set_color normal
+	
+	# GIT
+	echo (__terlar_git_prompt)
+	
+	
+	# Face
+	echo -e  "$face \c"
+	set_color normal
 
-# Time
-set_color blue
-echo -e  (date "+%m/%d") "\c"
-
-set_color green
-echo -e  (date "+%a") "\c"
-
-set_color yellow
-echo -e  (date "+%H:%M:%S")"\c"
-
-set_color FF8C00
-echo -e "+1s⌚⌚ \c"
-
-# PWD
-set_color red
-echo -n  (prompt_pwd)
-set_color normal
-
-# GIT
-echo (__terlar_git_prompt)
-
-
-# Face
-echo -e  "$face \c"
-set_color normal
 end
 
 
