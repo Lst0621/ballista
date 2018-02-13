cebc9819b6126308fa29ba5bad8c2d3feb3523b5
Haojun Lu <haojunlu@foxmail.com>
2018-02-13 01:19:21 -0500

    add triangle/arrow. need powerline font

--- fish/config.fish
+++ fish/config.fish
@@ -15,34 +15,63 @@ eval (thefuck --alias | tr '
 set face (python ~/workspace/ballista/helper/python/prompt.pyc)
 set myname (whoami)
 
+# powerline font needed!
+# ref: https://askubuntu.com/questions/283908/how-can-i-install-and-use-powerline-plugin
+set segment_separator \uE0B0
+
 
 function fish_prompt
+	
+	
 
 	# Name
 	set_color 000000
-	set_color -b 9400d3
-	echo  -e "$myname\c"
-	set_color -b 4B0082
-	echo  -e "@\c"
+	set -g current_bg 9400d3
+	set_color -b $current_bg
+	echo  -n "$myname"
+
+
+	set -g current_bg  4B0082
+	set_color -b $current_bg
+
+	echo  -n "@"
+
 	
 	# Time
 	set_color  -b blue
-	echo -e  (date "+%m/%d")"\c"
+
+
+	set_color $current_bg
+	echo -n "$segment_separator"
+	set_color 000000
+
+
+	echo -n (date "+%m/%d")
 	
 	set_color -b green
-	echo -e  (date "+%a")"\c"
+	echo -n  (date "+%a")
 	
 	set_color -b yellow
-	echo -e  (date "+%H:%M:%S")"\c"
+	echo -n  (date "+%H:%M:%S")
 	
 	set_color -b EE8C00
-	echo -e "+1s⌚⌚ \c"
+	echo -n "+1s⌚⌚ "
 	
 	# PWD
 	set_color -b red
+	set_color  EE8C00
+
+	echo -n "$segment_separator"
+
+	set_color 000000
+
 	echo -n  (prompt_pwd)
-	set_color normal
+
+	set_color red
+	set_color -b 000000
+	echo -n "$segment_separator"
 	
+	set_color normal
 	# GIT
 	echo (__terlar_git_prompt)
 	
