1e12a105bb6ad941bee8c8ff11f136cc28f1eacf
Haojun Lu <haojunlu@foxmail.com>
2018-02-14 16:27:38 -0500

    use prompt_segment to show arrow

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -8,6 +8,7 @@ function fish_prompt --description 'Write out the prompt'
 	echo  -n "$myname"
 
 
+
 	set -g current_bg  4B0082
 	set_color -b $current_bg
 
@@ -16,60 +17,67 @@ function fish_prompt --description 'Write out the prompt'
 	
 	# Time
 
+	prompt_segment blue 000000 ''
 
-	set_color  -b blue
+	#set_color  -b blue
 
 
-	set_color $current_bg
-	echo -n "$segment_separator"
-	set_color 000000
+	#set_color $current_bg
+	#echo -n "$segment_separator"
+	#set_color 000000
 
 
 	echo -n (date "+%m/%d")
 
 
 	
-	set_color -b green
-	set_color blue
-	echo -n "$segment_separator"
-	set_color 000000
+	#set_color -b green
+	#set_color blue
+	#echo -n "$segment_separator"
+	#set_color 000000
 
+	prompt_segment green 000000 ''
 
 
 	echo -n  (date "+%a")
 
-	set_color green
-	set_color -b yellow
-	echo -n "$segment_separator"
-	set_color 000000
+	#set_color green
+	#set_color -b yellow
+	#echo -n "$segment_separator"
+	#set_color 000000
+
+	prompt_segment yellow 000000 ''
 
 	echo -n  (date "+%H:%M:%S")
 	
-	set_color -b EE8C00
-	set_color yellow
-	echo -n "$segment_separator"
+	#set_color -b EE8C00
+	#set_color yellow
+	#echo -n "$segment_separator"
+	#set_color 000000
 
-	set_color 000000
+	prompt_segment EE8C00 000000 ''
 	echo -n "+1s⌚⌚ "
+
 	
 	# PWD
-	set_color -b red
-	set_color  EE8C00
+	#set_color -b red
+	#set_color  EE8C00
+	#echo -n "$segment_separator"
+	#set_color 000000
 
-	echo -n "$segment_separator"
-
-	set_color 000000
-
+	prompt_segment red 000000 ''
 	echo -n  (prompt_pwd)
 
-	set_color red
-	set_color -b 000000
-	echo -n "$segment_separator"
-	
-	set_color normal
+	#set_color red
+	#set_color -b 000000
+	#echo -n "$segment_separator"
+	#set_color normal
+
+	prompt_segment 000000 000000 ''
 	
 	echo ""
 	
+	set_color normal
 	
 	# Face
 	echo -e  "$face \c"
